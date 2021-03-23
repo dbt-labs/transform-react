7f2955ff6d9e154ac05b2858423e8b5b287cfec5
adamrneary <adamrneary@gmail.com>
2021-03-23 10:51:25 -0700

    Schema updates

--- scripts/schemaCore.js
+++ scripts/schemaCore.js
@@ -8,15 +8,23 @@ const {
 } = require("graphql");
 
 process.env.NODE_ENV = "development";
+// Ensure environment variables are read.
+require("./loadEnvLocal");
+
+if (!process.env.API_TOKEN) {
+  throw new Error("No production token available in process.env.API_TOKEN");
+}
 
 async function main() {
   let response;
   let data;
   try {
-    response = await fetch("https://tfd-mql.prod.transformdata.io/graphql", {
+    response = await fetch("https://transform-staging.hasura.app/v1/graphql", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        Authorization: process.env.API_TOKEN,
+        "X-Hasura-Role": "admin",
       },
       body: JSON.stringify({ query: getIntrospectionQuery() }),
     });
@@ -30,13 +38,14 @@ async function main() {
     return Promise.reject(err);
   }
 
+  // GraphQL responses sometimes contain an errors array as part of a successful (200 status) response
   if (data.errors && data.errors.length > 0) {
     return Promise.reject(data.errors[0].message);
   }
 
   const schema = buildClientSchema(data.data);
 
-  const outputFile = path.join(__dirname, "../schemas/mql/mql.graphql");
+  const outputFile = path.join(__dirname, "../schemas/core/core.graphql");
 
   try {
     await fs.promises.writeFile(outputFile, printSchema(schema));
