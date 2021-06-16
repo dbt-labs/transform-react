76eaca64f123277c772608a05ba46a67218ef21a
Jenna Rajani <jenna@transformdata.io>
2021-06-15 20:25:07 -0700

    Update SetMqlServer.ts

--- mutations/core/SetMqlServer.ts
+++ mutations/core/SetMqlServer.ts
@@ -2,15 +2,9 @@ import { gql } from "urql";
 
 const mutation = gql`
   mutation SetMqlServer($newServerIdAsString: String!) {
-    createUserPreference(
-      object: {
-        prefKey: "mql_server_override_id"
-        prefValue: $newServerIdAsString
-      }
-      on_conflict: {
-        constraint: user_prefs_user_id_pref_key_key
-        update_columns: prefValue
-      }
+    setUserPreference(
+      prefKey: "mql_server_override_id"
+      prefValue: $newServerIdAsString
     ) {
       id
       prefKey
