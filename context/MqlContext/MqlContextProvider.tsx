aaf3fe54af25f62b17ce65ac006e31a9440b682a
Jenna Rajani <jenna@transformdata.io>
2021-08-26 17:27:43 -0400

    fix mql server override prop

--- context/MqlContext/MqlContextProvider.tsx
+++ context/MqlContext/MqlContextProvider.tsx
@@ -1,3 +1,193 @@
+// import React, { useEffect, useState, ReactNode, useCallback } from "react";
+// import { useQuery, useMutation, CombinedError, Provider } from "urql";
+//
+// import buildMqlUrqlClient from "../../utils/builMqlUrqlClient";
+// import MqlServerUrlQuery from "../../queries/core/MqlServerUrlQuery";
+// import { MqlServerUrlQuery as MqlServerUrlQueryType } from "../../queries/core/CoreApiQueryTypes";
+// import SetMqlServerMutation from "../../mutations/core/SetMqlServer";
+// import {
+//   SetMqlServerMutation as SetMqlServerMutationType,
+//   SetMqlServerMutationVariables,
+// } from "../../mutations/core/CoreApiMutationTypes";
+// import MqlContext, { HandleCombinedErrorContext, MqlContextType, CORE_API_URL } from "./MqlContext";
+// import getUserIdFromAuthToken from "./utils/getUserIdFromAuthToken";
+//
+// type Props = {
+//   getToken: () => Promise<string>;
+//   isAuthenticated: boolean;
+//   captureException?: (e: CombinedError, context: HandleCombinedErrorContext) => void;
+//   children: ReactNode;
+//   coreApiUrl?: string;
+//   mqlServerUrlOverride?: string;
+// };
+//
+// /*
+//   This thin wrapper allows us to use an urql Provider to provide access to our Core API
+//   for the sake of interacting with MQL Server and Model Key selection
+// */
+// function MqlContextProvider({
+//   getToken,
+//   children,
+//   isAuthenticated,
+//   captureException,
+//   coreApiUrl,
+//   mqlServerUrlOverride
+// }: Props) {
+//   const coreApiClient = buildMqlUrqlClient(getToken, coreApiUrl || CORE_API_URL);
+//   return (
+//     <Provider value={coreApiClient}>
+//       <MqlContextProviderInternal
+//         isAuthenticated={isAuthenticated}
+//         getToken={getToken}
+//         captureException={captureException}
+//         coreApiUrl={coreApiUrl}
+//         mqlServerUrlOverride={mqlServerUrlOverride}
+//       >
+//         {children}
+//       </MqlContextProviderInternal>
+//     </Provider>
+//   );
+// }
+//
+// function MqlContextProviderInternal({
+//   getToken,
+//   isAuthenticated,
+//   children,
+//   captureException,
+//   coreApiUrl,
+//   mqlServerUrlOverride
+// }: Props) {
+//   // We do this because we want to allow the parent to provide a bespoke error handling function.
+//   const handleCombinedError = useCallback(
+//     (e: CombinedError, context?: HandleCombinedErrorContext) => {
+//       const errFunction = captureException || console.error;
+//       if (e.message) {
+//         errFunction(e.message, context);
+//       }
+//       if (e.networkError) {
+//         errFunction(e.networkError, context);
+//       }
+//     },
+//     [captureException]
+//   );
+//
+//   /*
+//     Until recently, making a request to the MQL Server would require:
+//       1. The MQL Server URL
+//       2. A specific Model Key representing the model content used to execute the query
+//     Because we cannot yet specify the desired model in the UI, we would always retrieve
+//     and then supply the current model key. But now the MQL Server considers the Model Key
+//     optional. In its absense, it uses the current model.
+//     In the future, our UI will allow users to select a specific Model Key for querying,
+//     and at that point, we will update this code with a setModelKey mutation as we have
+//     for MqlServerUrl.
+//   */
+//   const [
+//     { data: mqlServerUrlData, error: mqlServerUrlError },
+//     refetchMqlServerUrl,
+//   ] = useQuery<MqlServerUrlQueryType>({
+//     query: MqlServerUrlQuery,
+//     pause: !isAuthenticated,
+//   });
+//   if (mqlServerUrlError) handleCombinedError(mqlServerUrlError);
+//
+//   const [{ fetching }, setMqlServer] = useMutation<
+//     SetMqlServerMutationType,
+//     SetMqlServerMutationVariables
+//   >(SetMqlServerMutation);
+//
+//   const setMqlServerThenRefetch = useCallback(
+//     async (newServerId: number) => {
+//       const token = await getToken();
+//       // Note: newServerid is cast as a string because it is stored on the backend
+//       // as a User Preference, whose values are all strings.
+//       const userId = getUserIdFromAuthToken(token);
+//       if (userId) {
+//         return setMqlServer({
+//           newServerIdAsString: `${newServerId}`,
+//           userId,
+//         }).then(refetchMqlServerUrl);
+//       }
+//     },
+//
+//     [setMqlServer, refetchMqlServerUrl]
+//   );
+//
+//   /*
+//     Note: Before the mqlServerUrl is available, an urql client will be constructed with the url set to the CORE_API_URL.
+//     This is because it throws an error to build an urql client without a URL.
+//     Naturally, this is invalid, so we must check with the mqlServerUrl is present before initiating MQL Queries.
+//   */
+//   const mqlClient = buildMqlUrqlClient(
+//     getToken,
+//     mqlServerUrlData?.mqlServerUrl || coreApiUrl || CORE_API_URL
+//   );
+//
+//   const [mqlContext, setMqlContext] = useState<MqlContextType>({
+//     coreApiUrl: CORE_API_URL,
+//     mqlServerUrl: mqlServerUrlData?.mqlServerUrl,
+//     setMqlServer: setMqlServerThenRefetch,
+//     mqlServerOverrideLoading: fetching,
+//     modelKey: null,
+//     setModelKey: () => Promise.resolve(),
+//     modalKeyOverrideLoading: false,
+//     mqlClient,
+//     getToken,
+//     useQuery,
+//     useMutation,
+//     handleCombinedError,
+//   });
+//
+//   useEffect(() => {
+//     const stateToUpdate: Partial<MqlContextType> = {};
+//     const useOverride = !!mqlServerUrlOverride;
+//     if (useOverride && isAuthenticated) {
+//       if (mqlServerUrlOverride !== mqlContext.mqlServerUrl) {
+//         stateToUpdate.mqlServerUrl = mqlServerUrlOverride;
+//       }
+//       if (
+//         mqlServerUrlOverride &&
+//         mqlServerUrlOverride !== mqlContext.mqlServerUrl
+//       ) {
+//         stateToUpdate.mqlClient = buildMqlUrqlClient(
+//           getToken,
+//           mqlServerUrlOverride
+//         );
+//       }
+//     } else {
+//       if (mqlServerUrlData?.mqlServerUrl !== mqlContext.mqlServerUrl) {
+//         stateToUpdate.mqlServerUrl = mqlServerUrlData?.mqlServerUrl;
+//       }
+//       if (
+//         mqlServerUrlData?.mqlServerUrl &&
+//         mqlServerUrlData?.mqlServerUrl !== mqlContext.mqlServerUrl
+//       ) {
+//         stateToUpdate.mqlClient = buildMqlUrqlClient(
+//           getToken,
+//           mqlServerUrlData?.mqlServerUrl
+//         );
+//       }
+//   }
+//     /*
+//       FUTURE: Add check for Model Key.
+//     */
+//
+//     if (Object.keys(stateToUpdate).length > 0) {
+//       setMqlContext({
+//         ...mqlContext,
+//         ...stateToUpdate,
+//       });
+//     }
+//   }, [mqlServerUrlData, getToken, mqlContext]);
+//
+//   return (
+//     <Provider value={mqlContext.mqlClient}>
+//       <MqlContext.Provider value={mqlContext}>{children}</MqlContext.Provider>
+//     </Provider>
+//   );
+// }
+//
+// export default MqlContextProvider;
 import React, { useEffect, useState, ReactNode, useCallback } from "react";
 import { useQuery, useMutation, CombinedError, Provider } from "urql";
 
@@ -122,7 +312,7 @@ function MqlContextProviderInternal({
   */
   const mqlClient = buildMqlUrqlClient(
     getToken,
-    mqlServerUrlOverride || mqlServerUrlData?.mqlServerUrl || coreApiUrl || CORE_API_URL
+    mqlServerUrlData?.mqlServerUrl || coreApiUrl || CORE_API_URL
   );
 
   const [mqlContext, setMqlContext] = useState<MqlContextType>({
@@ -145,7 +335,7 @@ function MqlContextProviderInternal({
   useEffect(() => {
     const stateToUpdate: Partial<MqlContextType> = {};
     const useOverride = !!mqlServerUrlOverride;
-    if (useOverride) {
+    if (useOverride && isAuthenticated) {
       if (mqlServerUrlOverride !== mqlContext.mqlServerUrl) {
         stateToUpdate.mqlServerUrl = mqlServerUrlOverride;
       }
