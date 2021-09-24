b129732dc49c0d32fa2590f205883bb10b1c4531
He1DAr <heidar@transformdata.io>
2021-09-23 21:56:13 -0400

    Add FetchDimensionNamesMultihop query

--- queries/mql/index.ts
+++ queries/mql/index.ts
@@ -1,4 +1,5 @@
 export { default as FetchDimensionNames } from "./FetchDimensionNames";
+export { default as FetchDimensionNamesMultihop } from "./FetchDimensionNamesMultihop";
 export { default as FetchDimensionValues } from "./FetchDimensionValues";
 export { default as FetchMqlQueryLogs } from "./FetchMQLQueryLogs";
 export { default as FetchMqlQueryLog } from "./FetchMQLQueryLog";
