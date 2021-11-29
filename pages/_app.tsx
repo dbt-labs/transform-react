0295b647a6fee8e792a7e54a1e4356f0261b4fde
Jenna Rajani <jenna@transformdata.io>
2021-11-29 09:23:53 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,10 @@ import React from "react";
 import "../styles/globals.css";
 import MqlContextProvider from "../context/MqlContext/MqlContextProvider";
 
-// Note: In a production system, this might include the ability to fetch from a cookie
-// or fetch remotely, re-validate, etc.
-function getToken() {
-  return Promise.resolve(`${process.env.NEXT_PUBLIC_TOKEN}`);
-}
-
 // @ts-ignore
 export default function TransformReactDemo({ Component, pageProps }) {
   return (
-    <MqlContextProvider getToken={getToken} isAuthenticated>
+    <MqlContextProvider token={process.env.NEXT_PUBLIC_TOKEN} isAuthenticated>
       <Component {...pageProps} />
     </MqlContextProvider>
   );
