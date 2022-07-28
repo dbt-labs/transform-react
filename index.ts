70637291d31d63b9dddfb1eb8a04360ca8fd6b66
Conglei Shi <conglei@transformdata.io>
2022-07-28 07:47:19 -0700

    tiny

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import MqlContext from "context/MqlContext/MqlContext";
-import MqlContextProvider from "context/MqlContext/MqlContextProvider";
-import { AcceptedHeaders } from "context/MqlContext/MqlContextProvider";
+import MqlContextProvider, { AcceptedHeaders } from "context/MqlContext/MqlContextProvider";
 
 export default {
   MqlContext,
