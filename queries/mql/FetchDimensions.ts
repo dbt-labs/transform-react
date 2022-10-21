b21e9d246a950df882596245b467e5dedfc97f20
Jenna Rajani <jenna@transformdata.io>
2022-10-21 14:26:09 -0400

    include all arg for fetching dimensions for metrics

--- queries/mql/FetchDimensions.ts
+++ queries/mql/FetchDimensions.ts
@@ -1,24 +1,24 @@
-import { gql } from "urql";
+import { gql } from 'urql';
 
 const query = gql`
-  query FetchDimensions($metricNames: [String!]!) {
-    dimensionsForMetrics(metricNames: $metricNames) {
-        localDimensions {
-            name
-            identifierName
-            type
-            isPrimaryTime
-            values
-            cardinality
-        }
-        nonLocalDimensions {
-            name
-            identifierName
-            type
-            isPrimaryTime
-            values
-            cardinality
-        }
+  query FetchDimensions($metricNames: [String!]!, $includeAll: Boolean) {
+    dimensionsForMetrics(metricNames: $metricNames, includeAll: $includeAll) {
+      localDimensions {
+        name
+        identifierName
+        type
+        isPrimaryTime
+        values
+        cardinality
+      }
+      nonLocalDimensions {
+        name
+        identifierName
+        type
+        isPrimaryTime
+        values
+        cardinality
+      }
     }
   }
 `;
